a4471818a18ce03358d135c202ed5bbcbf52411e
Gergely Kiss <gergelytamas.kiss@gmail.com>
2022-11-18 21:21:29 +0100

    Initial Commit

---
+++ ToolBox/ToolBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolBoxApp.swift
+//  ToolBox
+//
+//  Created by Gergely Kiss on 2022. 11. 18..
+//
+
+import SwiftUI
+
+@main
+struct ToolBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
